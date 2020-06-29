f387ba55a0ba47b2247af499ce96990c56442b54
Amit Sharma <amit.sharma@srijan.net>
2020-06-29 16:30:05 +0530

    Modifying the anchore file to read vulnerabilities.

---
+++ scripts/anchore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#     if [v.severity == 'high' && fix != 'none']
+
+
+# jq -c '.[]' ./anchore-reports/scan_php_cli.json | while read i; do
+#    echo ${i};
+# done
+
+ls ./anchore-reports/
